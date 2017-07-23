77bec73f1abff2fae21ab78837b54c9e48ddccde
Syam Sathyan <syam.sathyan@adp.com>
2017-07-23 15:35:40 -0400

    updated Ignore

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Pass
-curl -H 'Content-Type: application/json'   -d '{"user": "Syam"}' localhost:3000
+curl -H 'Content-Type: application/json'   -d '{"asset": "test", "user": "Syam"}' localhost:3000
 
-#Fail for body without 'user'
+#Fail for body without 'user' and asset
 curl -H 'Content-Type: application/json' -d '{"name": "user"}' localhost:3000
